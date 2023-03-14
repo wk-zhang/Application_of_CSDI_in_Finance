bd8be0df0afc8e6ae0debd5d3edffc637c309347
loading12321 <80676412+loading12321@users.noreply.github.com>
2023-03-14 15:05:44 +0800

    Update dataset_physio.py

--- dataset_physio.py
+++ dataset_physio.py
@@ -1,133 +1,68 @@
 import pickle
-
-import os
-import re
-import numpy as np
-import pandas as pd
 from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+import numpy as np
+import torch
+from tqdm import tqdm
+from datetime import datetime
+from dataset_physio import get_dataloader
 
-# 35 attributes which contains enough non-values
-attributes = ['DiasABP', 'HR', 'Na', 'Lactate', 'NIDiasABP', 'PaO2', 'WBC', 'pH', 'Albumin', 'ALT', 'Glucose', 'SaO2',
-              'Temp', 'AST', 'Bilirubin', 'HCO3', 'BUN', 'RespRate', 'Mg', 'HCT', 'SysABP', 'FiO2', 'K', 'GCS',
-              'Cholesterol', 'NISysABP', 'TroponinT', 'MAP', 'TroponinI', 'PaCO2', 'Platelets', 'Urine', 'NIMAP',
-              'Creatinine', 'ALP']  # TODO change to stock attributes
-
-
-def extract_hour(x):
-    h, _ = map(int, x.split(":"))
-    return h
-
-
-def parse_data(x):
-    # extract the last value for each attribute
-    x = x.set_index("Parameter").to_dict()["Value"]
-
-    values = []
-
-    for attr in attributes:
-        if x.__contains__(attr):
-            values.append(x[attr])
-        else:
-            values.append(np.nan)
-    return values
-
-
-def parse_id(id_, missing_ratio=0.1):
-    data = pd.read_csv("./data/physio/set-a/{}.txt".format(id_))
-    # set hour
-    data["Time"] = data["Time"].apply(lambda x: extract_hour(x))
-
-    # create data for 48 hours x 35 attributes
-    observed_values = []
-    for h in range(48):
-        observed_values.append(parse_data(data[data["Time"] == h]))
-    observed_values = np.array(observed_values)
-    observed_masks = ~np.isnan(observed_values)
-
-    # randomly set some percentage as ground-truth
-    masks = observed_masks.reshape(-1).copy()
-    obs_indices = np.where(masks)[0].tolist()
-    miss_indices = np.random.choice(
-        obs_indices, (int)(len(obs_indices) * missing_ratio), replace=False
-    )
-    masks[miss_indices] = False
-    gt_masks = masks.reshape(observed_masks.shape)
-
-    observed_values = np.nan_to_num(observed_values)
-    observed_masks = observed_masks.astype("float32")
-    gt_masks = gt_masks.astype("float32")
-
-    return observed_values, observed_masks, gt_masks
-
-
-# TODO idlist to date
-def get_idlist():
-    patient_id = []
-    for filename in os.listdir("./data/physio/set-a"):
-        match = re.search("\d{6}", filename)
-        if match:
-            patient_id.append(match.group())
-    patient_id = np.sort(patient_id)
-    return patient_id
-
-
-class Physio_Dataset(Dataset):
-    def __init__(self, eval_length=48, use_index_list=None, missing_ratio=0.0, seed=0):
+class FINA_Dataset(Dataset):
+    
+    def __init__(self, eval_length, use_index_list = None, missing_ratio = 0.1, seed = 0):
         self.eval_length = eval_length
-        np.random.seed(seed)  # seed for ground truth choice
-
+        self.target_dim = target_dim
+        np.random.seed(seed)
+        
+        df = pd.read_csv('/home/sida/wmx/CSDI-main/CSDI-main/data/885457.TI.df.csv',index_col = 'date')  # full data
+        df_gt = pd.read_csv('/home/sida/wmx/CSDI-main/CSDI-main/data/885457.TI.df_gt.csv',index_col = 'date')  # missing data
+        n_columns = len(df.columns)
+        
         self.observed_values = []
         self.observed_masks = []
         self.gt_masks = []
         path = (
             "./data/physio_missing" + str(missing_ratio) + "_seed" + str(seed) + ".pk"
         )
-
-        if os.path.isfile(path) == False:  # if datasetfile is none, create
-            idlist = get_idlist()  # TODO change to get date
-            for id_ in idlist:
-                try:
-                    observed_values, observed_masks, gt_masks = parse_id(
-                        id_, missing_ratio
-                    )
-                    self.observed_values.append(observed_values)
-                    self.observed_masks.append(observed_masks)
-                    self.gt_masks.append(gt_masks)
-                except Exception as e:
-                    print(id_, e)
-                    continue
-            self.observed_values = np.array(self.observed_values)
-            self.observed_masks = np.array(self.observed_masks)
-            self.gt_masks = np.array(self.gt_masks)
-
-            # calc mean and std and normalize values
-            # (it is the same normalization as Cao et al. (2018) (https://github.com/caow13/BRITS))
-            tmp_values = self.observed_values.reshape(-1, 35)
-            tmp_masks = self.observed_masks.reshape(-1, 35)
-            mean = np.zeros(35)
-            std = np.zeros(35)
-            for k in range(35):
-                c_data = tmp_values[:, k][tmp_masks[:, k] == 1]
-                mean[k] = c_data.mean()
-                std[k] = c_data.std()
-            self.observed_values = (
-                (self.observed_values - mean) / std * self.observed_masks
+        
+        date = df.index.unique()
+        for i in date:
+            temp = df.loc[[i]]
+            observed_values = np.array(temp)
+            observed_masks = ~np.isnan(observed_values) # take the negation of np.isnan
+            gt_masks = ~np.isnan(np.array(df_gt.loc[[i]]))
+        
+            self.observed_values.append(observed_values)
+            self.observed_masks.append(observed_masks)
+            self.gt_masks.append(gt_masks)
+            
+        self.observed_values = np.array(observed_values)
+        self.observed_masks = np.array(observed_masks)
+        self.gt_masks = np.array(self.gt_masks)
+        
+        
+        tmp_values = self.observed_values.reshape(-1,n_columns)
+        tmp_masks = self.observed_masks.reshape(-1,n_columns)
+        mean = np.zeros(n_columns)
+        std = np.zeros(n_columns)
+        for k in range(n_columns):
+            c_data = tmp_values[:, k][tmp_masks[:, k]==1]
+            mean[k] = c_data.mean()
+            std[k] = c_data.std()
+        self.observed_values = (
+            (self.observed_values - mean)/std * self.observed_masks
+        )
+        
+        with open(path, "wb") as f:
+            pickle.dump(
+                [self.observed_values,self.observed_masks,self.gt_masks], f
             )
-
-            with open(path, "wb") as f:
-                pickle.dump(
-                    [self.observed_values, self.observed_masks, self.gt_masks], f
-                )
-        else:  # load datasetfile
-            with open(path, "rb") as f:
-                self.observed_values, self.observed_masks, self.gt_masks = pickle.load(
-                    f
-                )
+        
         if use_index_list is None:
             self.use_index_list = np.arange(len(self.observed_values))
         else:
             self.use_index_list = use_index_list
-
+            
     def __getitem__(self, org_index):
         index = self.use_index_list[org_index]
         s = {
@@ -145,7 +80,7 @@ class Physio_Dataset(Dataset):
 def get_dataloader(seed=1, nfold=None, batch_size=16, missing_ratio=0.1):
 
     # only to obtain total length of dataset
-    dataset = Physio_Dataset(missing_ratio=missing_ratio, seed=seed)
+    dataset = FINA_Dataset(missing_ratio=missing_ratio, seed=seed)
     indlist = np.arange(len(dataset))
 
     np.random.seed(seed)
@@ -163,7 +98,7 @@ def get_dataloader(seed=1, nfold=None, batch_size=16, missing_ratio=0.1):
     train_index = remain_index[:num_train]
     valid_index = remain_index[num_train:]
 
-    dataset = Physio_Dataset(
+    dataset = FINA_Dataset(
         use_index_list=train_index, missing_ratio=missing_ratio, seed=seed
     )
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=1)
